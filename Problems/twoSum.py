024a831845fba67deb8bd065fe8f5c8fed0b8b28
pairofAces <karanchauhan16@gmail.com>
2021-02-20 17:07:41 -0500

    inserted the two number sum prompt

---
+++ Problems/twoSum.py
@@ -0,0 +1,8 @@
+# Two Number Sum
+
+# given a non-empty array of distinct integers and an 
+# integer representing a target sum
+    # if any 2 numbers add up to the target integer
+        # return the 2 numbers in an array
+    # if no numbers add up to the target
+        # return an empty array
