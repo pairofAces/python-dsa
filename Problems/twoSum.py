903272d384c677b78853d4b3196469bbac49842a
pairofAces <karanchauhan16@gmail.com>
2021-02-20 17:45:58 -0500

    added solution 1 with time/space complexity

--- Problems/twoSum.py
+++ Problems/twoSum.py
@@ -7,4 +7,14 @@
     # if no numbers add up to the target
         # return an empty array
 
+# Solution 1
+# O(n^2) time | O(1) space
 def twoNumberSum(array, targetSum):
+    for i in range(len(array) - 1):
+        firstNum = array[i]
+        for j in range(i + 1, len(array)):
+            secondNum = array[j]
+            if firstNum + secondNum == targetSum:
+                return [firstNum, secondNum]
+    return []
+
