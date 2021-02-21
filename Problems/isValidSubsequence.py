a1f7922a9a8921a81f7c189aee15df537f4e0939
pairofAces <karanchauhan16@gmail.com>
2021-02-21 17:33:42 -0500

    created validate subsequence question template

---
+++ Problems/isValidSubsequence.py
@@ -0,0 +1,10 @@
+# Validate Subsequence
+
+# Prompt:
+    # Given two non-empty array of integers, create a function that 
+    # determines whether the second array is a subsequence of the 
+    # first one.
+
+# Subsequence: a set of numbers that don't have to be adjacent in
+# the array but that are in the same order as they appear in the 
+# initial array.
