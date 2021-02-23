83b32956f9bc6c51af4058270b1f1ee6bcc4bd8f
pairofAces <karanchauhan16@gmail.com>
2021-02-22 22:00:27 -0500

    added three number sum prompt

---
+++ Problems/Medium/threeNumberSum.py
@@ -0,0 +1,9 @@
+# Three Number Sum
+
+# Prompt:
+# create a function that takes in a non-empty array of distinct integers
+# and a target sum.
+    # function should find all triplets in the array that add up to the 
+    # target integer
+        # the triplets need to be returned in an array
+
