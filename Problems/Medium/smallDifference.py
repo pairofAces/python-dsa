2cfda3d5fe0555aca88bd40e41631f893d523b33
pairofAces <karanchauhan16@gmail.com>
2021-03-05 21:12:06 -0500

    sorted the input arrays and created the necessary variables

--- Problems/Medium/smallDifference.py
+++ Problems/Medium/smallDifference.py
@@ -7,4 +7,14 @@
     # return the array containing these two numbers
     # first number should be from first array and second
     # number should be from the second array
+def smallestDifference(arrayOne, arrayTwo):
+    # sort both arrays
+    arrayOne.sort()
+    arrayTwo.sort()
 
+    # create the variables
+    idxOne = 0
+    idxTwo = 0
+    smallest = Float("inf")
+    current = Float("inf")
+    smallestPair = []
