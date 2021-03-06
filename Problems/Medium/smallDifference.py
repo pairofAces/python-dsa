08c5003a1e9f7206a5c3ed60c4bac49ba94a598b
pairofAces <karanchauhan16@gmail.com>
2021-03-05 21:09:36 -0500

    added prompt for smallDifference.py

---
+++ Problems/Medium/smallDifference.py
@@ -0,0 +1,10 @@
+# SMALLEST DIFFERENCE
+# Prompt
+
+# create a function that takes two parameters, 2 non-empty
+# arrays of integers. The function will find the pair of numbers,
+# whos absolute difference is closest to 0.
+    # return the array containing these two numbers
+    # first number should be from first array and second
+    # number should be from the second array
+
