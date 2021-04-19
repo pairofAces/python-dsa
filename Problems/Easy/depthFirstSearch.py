efc51dacbc666ceea742ed0141806a97f1229f27
pairofAces <karanchauhan16@gmail.com>
2021-04-19 19:29:04 -0400

    created the Node class, now working on the dfs class method

--- Problems/Easy/depthFirstSearch.py
+++ Problems/Easy/depthFirstSearch.py
@@ -5,3 +5,15 @@
 # takes an empty array and taverses the tree, from left-to-right, storing 
 # all of the node names into the input array and return it. 
 
+# create the node class
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+    
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+    
+    def depthFirstSearch(self, array):
+        
