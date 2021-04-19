f1dd7258af25823acef8325c125625c40cda8f72
pairofAces <karanchauhan16@gmail.com>
2021-04-19 19:24:35 -0400

    working on depthFirstSearch in python

---
+++ Problems/Easy/depthFirstSearch.py
@@ -0,0 +1,7 @@
+# Depth-First Search
+
+# Given a Node class that has a name and an array of optional children
+# nodes, implement the depthFirstSearch method on the Node class, that
+# takes an empty array and taverses the tree, from left-to-right, storing 
+# all of the node names into the input array and return it. 
+
