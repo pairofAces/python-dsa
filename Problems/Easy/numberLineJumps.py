0ea24d065b6e44f8325156bde7f91a484e6dda74
pairofAces <karanchauhan16@gmail.com>
2021-04-20 21:55:53 -0400

    created solution

--- Problems/Easy/numberLineJumps.py
+++ Problems/Easy/numberLineJumps.py
@@ -10,3 +10,10 @@
     # - figure out a way to get both jkangaroos at the same location at the 
     # same time. If this is possible, return 'YES', otherwise 'NO'
 function kangaroo(x1, v1, x2, v2):
+    # use an if/else statement
+        # as long as v1 is greater than v2 AND the remainder of
+        # x2-x1 divided by v2-v1, is 0, then return a "YES"
+        if (v1 > v2) and ((x2-x1) % (v2-v1) == 0):
+            return "YES"
+        else:
+            return "NO"
