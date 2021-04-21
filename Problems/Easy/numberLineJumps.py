f41227377467d2f7a512503e299638b2c6f19684
pairofAces <karanchauhan16@gmail.com>
2021-04-20 21:53:24 -0400

    added numberLineJumps prompt and function template

---
+++ Problems/Easy/numberLineJumps.py
@@ -0,0 +1,12 @@
+# NUMBER LINE JUMPS
+
+# PROMPT
+
+# Given two kangaroos on a number line ready to jump in the positive 
+# direction
+    # - 1st kangaroo starts at location x1 and moves at v1 meters per jump.
+    # - 2nd kangaroo starts at location x2 and moves at v2 meters per jump.
+
+    # - figure out a way to get both jkangaroos at the same location at the 
+    # same time. If this is possible, return 'YES', otherwise 'NO'
+function kangaroo(x1, v1, x2, v2):
