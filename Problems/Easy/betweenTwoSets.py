e81c2bb712b2aa64461a25475dc9fd4a8528f98f
pairofAces <karanchauhan16@gmail.com>
2021-04-21 21:58:44 -0400

    created question and prompt for betweenTwoSets

---
+++ Problems/Easy/betweenTwoSets.py
@@ -0,0 +1,19 @@
+# Between Two Sets
+
+# Prompt
+
+# Given two arrays of integers
+# Determine all integers that satsfy the following conditions:
+    # - the elements of the first array are all factors of the integer
+    #   being considered
+
+    # - the integer being considered, is a factor of all elements in
+    #  the second array.
+
+# These numbers are referred to as being betweent the arrays (the
+# two sets)
+
+# Determine how may of these numbers exist?
+
+def getTotalX(a, b):
+    
