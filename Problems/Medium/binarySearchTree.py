f400a987e8e4555ccf63a35d01a06f25162e617c
pairofAces <karanchauhan16@gmail.com>
2021-04-24 18:30:36 -0400

    created class method templates

--- Problems/Medium/binarySearchTree.py
+++ Problems/Medium/binarySearchTree.py
@@ -6,4 +6,23 @@
     # searching for values with the contains method
 
 class BST:
+    # initialize
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
     
+    # create the inset method
+    def insert(self, value):
+
+        return self
+    
+    # create the remove method
+    def remove(self, value):
+
+        return self
+
+    # create the contains method
+    def contains(self, value):
+
+        pass
