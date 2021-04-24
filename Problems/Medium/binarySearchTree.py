b594eca0c4b6810f3e3f6d2e80295fd2ed3f4790
pairofAces <karanchauhan16@gmail.com>
2021-04-24 19:36:09 -0400

    created contains and insert class methods in python

--- Problems/Medium/binarySearchTree.py
+++ Problems/Medium/binarySearchTree.py
@@ -14,7 +14,21 @@ class BST:
     
     # create the inset method
     def insert(self, value):
+        # if the value is less than the originally initialized value
+        # then the left value will become the new, lower value
 
+        # else, if the value is greater than the initialized value, the
+        # right value will be set to the new, greater value
+        if value < self.value:
+            if self.left is None:
+                self.left = BST(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = BST(value)
+            else:
+                self.right.insert(value)
         return self
     
     # create the remove method
@@ -24,5 +38,25 @@ class BST:
 
     # create the contains method
     def contains(self, value):
+        # if the value is less than the initialized value
+        if value < self.value:
+            # if there is no left value, then False
+            if self.left is None:
+                return False
+            else:
+            # otherwise, check if the left has the given value
+                return self.left.contains(value)
+                
+        # if the value is greater than the initialized value
+        elif value > self.value:
+            # if there is no right value, then False
+            if self.right is None:
+                return False
+            
+            # otherwise, check if the right has the given value
+            else:
+                return self.right.contains(value)
 
-        pass
+        # if the given value is found, then True
+        else:
+            return True
