f839ab28b463187fbe2908df3391164c90024c99
pairofAces <karanchauhan16@gmail.com>
2021-04-24 18:28:11 -0400

    working on creating binary search tree class in python

---
+++ Problems/Medium/binarySearchTree.py
@@ -0,0 +1,9 @@
+# Binary Search Tree Construction
+
+# create a BST class that supports the following:
+    # inserting values with the insert method
+    # removing values with the remove method
+    # searching for values with the contains method
+
+class BST:
+    
