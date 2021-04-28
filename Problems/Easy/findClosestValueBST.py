ee87e93aaf6caaf5f1f65ad08824a2299ff0248d
pairofAces <karanchauhan16@gmail.com>
2021-04-27 21:00:33 -0400

    working on inputting the solution to finding closest value in BST in python

---
+++ Problems/Easy/findClosestValueBST.py
@@ -0,0 +1,10 @@
+# Find the closest value in Binary Search Tree
+
+# create a function that takes in a BST and a target integer value
+# and return the value that's closest to the target integer in the BST.
+
+    # assume there's only one closest value
+
+def findClosestValueInBst(tree, target):
+    
+
