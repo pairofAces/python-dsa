3dff3da7692adc161855b5be3752634be06fa4a0
pairofAces <karanchauhan16@gmail.com>
2021-04-29 20:05:35 -0400

    generating solution to tounament winner in python

---
+++ Problems/Easy/tournamentWinner.py
@@ -0,0 +1,22 @@
+# Tournament Winner
+
+# Prompt 
+
+# Solution
+
+# function should take two parameters, competition and results
+def tournamentWinner(competition, results):
+    # create initial variable set as empty string
+    currentBest = ""
+
+    # create variable to represent scores, set to 0
+    scores = {currentBest: 0}
+
+    # initiate a for loop
+    for i, competiton in enumerate(competitions):
+        # create a result variable set to the i'th element in (results) input
+        result = results[i]
+        homeTeam, awayTeam = competition
+
+        # create a variable to represent the winning team
+        winningTeam = homeTeam if result == HOME_TEAM_WON else awayTeam
