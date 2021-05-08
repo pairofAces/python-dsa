ec2c8e17ee17ea724be56ae11b6d1cf4bad202ac
pairofAces <karanchauhan16@gmail.com>
2021-05-07 20:06:09 -0400

    created question and function template

---
+++ Problems/Medium/moveElementToEnd.py
@@ -0,0 +1,14 @@
+# MOVE ELEMENT TO END
+
+# PROMPT
+
+# Given an array of integers and an integer, create a function that moves
+# all instances of that integer in the array to the end of the array and 
+# returns the array.
+
+# Note -> function should mutute the input array, the order of the other
+# integers in the array don't need to be maintained
+
+class Solution:
+    def moveElementToEnd(array, toMove):
+        
