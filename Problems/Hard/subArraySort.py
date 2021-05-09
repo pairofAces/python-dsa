6bd6ab6ed8f17a0d102f992da9bff3a405aa75c9
pairofAces <karanchauhan16@gmail.com>
2021-05-08 22:39:53 -0400

    working on solution to subArraySort)

--- Problems/Hard/subArraySort.py
+++ Problems/Hard/subArraySort.py
@@ -11,3 +11,17 @@
 
 class Solution:
     def subarraySort(array):
+        # create 2 variables, one for minimum and maximum
+        minNum = float('inf'')
+        maxNum = float('-inf')
+
+        # use a for loop relative to the aray's length
+        for i in range(len(array)):
+            # create a variable to be equal to the (i)th element in 
+            num = array[i]
+
+            # use an if statement, aspirational code -> helper function
+            if outOfOrder(i, num, array):
+
+# create helper function below
+def outOfOrder(i, num, array):
