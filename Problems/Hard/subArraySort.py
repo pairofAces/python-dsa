fce12b57c2c0ec7cf1e94632510a4cb972f5443e
pairofAces <karanchauhan16@gmail.com>
2021-05-08 22:32:25 -0400

    created question template

---
+++ Problems/Hard/subArraySort.py
@@ -0,0 +1,13 @@
+# Subarray Sort
+
+# The prompt
+
+# create a function that takes an array of at least 2 integers
+# and returns an array of the starting and ending indices of the smallest
+# subarray in the input array that needs to be sorted in place, in order
+# for the entire input array to be sorted, in ascending order. 
+    # Note - if the input array is already sorted then return 
+    #        [-1, -1]
+
+class Solution:
+    def subarraySort(array):
