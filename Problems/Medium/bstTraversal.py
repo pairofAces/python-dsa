0378d159e0874b2614709f65917349535a82cff9
pairofAces <karanchauhan16@gmail.com>
2021-05-15 15:30:44 -0400

    created question template and initated class Solution

---
+++ Problems/Medium/bstTraversal.py
@@ -0,0 +1,17 @@
+# BINARY SEARCH TREE TRAVERSAL
+
+create three functions that take in a Binary Search Tree (BST) and an 
+empty array , traverse the BST, add it's nodes values to the input
+array, and then return that array. 
+
+The functions should traverse the the BST using the in-order, 
+pre-order, and post-order tree-traversal techniques, respectively.
+
+class Solution:
+    def inOrderTraverse(tree, array):
+    
+
+    def preOrderTraverse(tree, array):
+
+
+    def postOrderTraversal(tree, array):
