ccb0ec5b4d27a3e7131f4b658d141236dfa95e26
pairofAces <karanchauhan16@gmail.com>
2021-05-20 22:02:11 -0400

    created question template for kadanes algorithm, working on solution

---
+++ Problems/Medium/kadanesAlgorithm.py
@@ -0,0 +1,13 @@
+# KADANES ALGORITHM
+
+# PROMPT
+
+# Create a function that takes a non-empty array of integers and returns
+# the maximum sum that can be obtained by summing up all of the integers 
+# in a non-empty subarray of the input array.
+
+# A subarray must only contain numbers that are adjacent to each other
+# in the input array
+
+def kadanesAlgorithm(array):
+    
