9d7dbb9203c039ed29186af2b0f44a65c6b6e3cb
pairofAces <karanchauhan16@gmail.com>
2021-05-21 23:12:12 -0400

    created question template, working on class Solution

---
+++ Problems/Medium/minHeightBst.py
@@ -0,0 +1,9 @@
+# Min Height Bst
+
+# create a function that takes in a non-empty sorted array of distinct integers
+# constructs a BST from the integers, and returns the root of the BST.
+
+# The function should minimize the height of the BST.
+
+class Solution:
+    def minHeightBst(array):
