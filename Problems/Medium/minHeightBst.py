ff911696a9b5cfd2552fa14eda20e9321e9b1ce5
pairofAces <karanchauhan16@gmail.com>
2021-05-21 23:15:58 -0400

    generated class BST, working on class Solution

--- Problems/Medium/minHeightBst.py
+++ Problems/Medium/minHeightBst.py
@@ -17,11 +17,11 @@ class BST:
     def insert(self, value):
         if value < self.value:
             if self.left is None:
-            
+                self.left = BST(value)
             else:
-
+                self.left.insert(value)
         else:
             if self.right is None:
-            
+                self.right = BST(value)
             else:
-                
+                self.right.insert(value)
