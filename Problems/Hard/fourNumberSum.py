36b498a43c7523a47a34de93b32aa74ef59e2c09
pairofAces <karanchauhan16@gmail.com>
2021-05-24 20:44:50 -0400

    created question template, working on class Solution for fourNumberSum.py

---
+++ Problems/Hard/fourNumberSum.py
@@ -0,0 +1,17 @@
+# FOUR NUMBER SUM
+
+# PROMPT
+
+# Create a function tha takes in a non-empty array of unique integers and an
+# integer that represents a target sum. 
+
+# The function should find all quadruplets in the array that sum up to the 
+# target sum, and return a two-dimensional array of all these quadruplets.
+
+# Note, order doesn't matter. 
+
+# If there aren't any quadruplets, the function should return an empty array. 
+
+class Solution:
+
+    def fourNumberSum(array, targetSum):
