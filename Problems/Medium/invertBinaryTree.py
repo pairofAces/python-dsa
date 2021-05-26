5e7538ac5d3b852b38a9704de8b41d7574bd0a0a
pairofAces <karanchauhan16@gmail.com>
2021-05-26 19:40:58 -0400

    created question template for inverting a binary tree, working on class Solution

---
+++ Problems/Medium/invertBinaryTree.py
@@ -0,0 +1,10 @@
+# INVERT BINARY TREE
+
+# PROMPT
+
+# create a function that takes in a Binary Tree and inverts it. 
+    # the function should swap every left node in the tree for its
+    # corresponding right node.
+
+class Solution:
+    def invertBinaryTree(tree):
