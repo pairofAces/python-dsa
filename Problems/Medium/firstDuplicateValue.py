e76d81eb529463954b8ff2232fb79d567bb86857
pairofAces <karanchauhan16@gmail.com>
2021-05-30 22:08:04 -0400

    created question template for firstDuplicateValue, working on class Solution

---
+++ Problems/Medium/firstDuplicateValue.py
@@ -0,0 +1,13 @@
+# First Duplicate Value
+
+# Prompt
+
+# Given an array of integers between 1 and n, inclusive, where n is the
+# length of the array, create a function that returns the first integer
+# that appears more than once. 
+    # note, only when the array is read from left to right
+    # if no integer is repeated, return a -1
+    # the input array can be mutated
+
+class Solution:
+    def findDuplicateValue(array):
