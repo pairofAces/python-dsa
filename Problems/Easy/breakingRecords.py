65fecf1d65faa7e9f6f8494e1f7019a215478832
pairofAces <karanchauhan16@gmail.com>
2021-06-04 22:29:18 -0400

    created question template and initiated solution function

---
+++ Problems/Easy/breakingRecords.py
@@ -0,0 +1,11 @@
+# HackerRank - Breaking Records
+
+# Prompt
+
+# Maria plays college basketball and wants to go pro. 
+# Each season she maintains a record of her play. She tabulates the 
+# number of times she breaks her season record for most points and 
+# least points in a game. Points scored in the first game establish her 
+# record for the season, and she begins counting from there.
+
+def breakingRecords(scores):
