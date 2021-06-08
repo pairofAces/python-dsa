aeeb43e0867e2e8787b652cacaf6a507fa061837
pairofAces <karanchauhan16@gmail.com>
2021-06-07 22:42:46 -0400

    working on class Solution for subarrayDivision

---
+++ Problems/Easy/subarrayDivision.py
@@ -0,0 +1,17 @@
+# HackerRank
+# Subarray Division a.k.a. Birthday Chocolate
+
+# Prompt
+
+# Lily and Ron want to share a choclate bar. Each square has an integer on
+# it. 
+
+# Lily decides to share a contiguous segment of the bar such that:
+    # the length of the segment matches Ron's birthday Month
+    # the sum of the integers on the squares is equal to Ron's birthdyay
+
+# Determine how many ways Lily can divide the bar
+
+class Solution:
+    def birthdays(s, d, m):
+        
