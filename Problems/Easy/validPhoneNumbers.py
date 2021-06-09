7fa0e81c56428ee4684aed869db60e7dc892658a
pairofAces <karanchauhan16@gmail.com>
2021-06-08 21:06:01 -0400

    created file template for validPhoneNumbers.py

---
+++ Problems/Easy/validPhoneNumbers.py
@@ -0,0 +1,11 @@
+# Valid Phone Numbers - Leetcode 193
+
+# Given a text file file.txt that contains a list of phone numbers
+# (one per line), create a one-liner bash script to print all the 
+# valud phone numbers,
+
+    # Note: the numbers should be displayed as either of the following:
+        # (nnn) nnn-nnnn
+        # nnn-nnn-nnn
+
+# Bash Script 
