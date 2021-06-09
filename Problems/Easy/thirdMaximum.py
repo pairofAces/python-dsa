0429e22143f9a3502d953f917d1666bc2033f9ff
pairofAces <karanchauhan16@gmail.com>
2021-06-09 19:42:49 -0400

    created question template for leetcode 414

---
+++ Problems/Easy/thirdMaximum.py
@@ -0,0 +1,9 @@
+# Third Maximum Number - LeetCode 414
+
+# Given an integer array, nums, return the third maximum number in the
+# input array. 
+    # If the third maximum doesn't exist, return the maximum
+
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        
