27a864dcbd8ea52a3b8ba165c653465ac86890eb
pairofAces <karanchauhan16@gmail.com>
2021-06-21 22:03:25 -0400

    reverseString, leetcode done

--- Problems/Easy/reverseString.py
+++ Problems/Easy/reverseString.py
@@ -4,6 +4,9 @@
 # the input string is provided as an array of
 # characters
 
+# Note: Don't return anything, modify (s) in-place
+
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        
+        # use python built-in method
+        s.reverse()
