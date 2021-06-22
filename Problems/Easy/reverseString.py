0c2b064a019c672d4083bf355dfc6477f2b6f92f
pairofAces <karanchauhan16@gmail.com>
2021-06-21 22:05:20 -0400

    added time/space complexity analysis

--- Problems/Easy/reverseString.py
+++ Problems/Easy/reverseString.py
@@ -6,6 +6,12 @@
 
 # Note: Don't return anything, modify (s) in-place
 
+# Complexity Analysis
+    # Time: O(n) time, where (n) is the lenght of the list
+
+    # Space: O(1) constant space, because no external data structures
+    # are being used
+
 class Solution:
     def reverseString(self, s: List[str]) -> None:
         # use python built-in method
