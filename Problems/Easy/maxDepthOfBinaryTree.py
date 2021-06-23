6f9e8f27c904dea29ada3c62804bbe129fbc4c33
pairofAces <karanchauhan16@gmail.com>
2021-06-22 23:35:51 -0400

    working on leetcode, maximum dept of binary tree

---
+++ Problems/Easy/maxDepthOfBinaryTree.py
@@ -0,0 +1,10 @@
+# Maximum Depth of Binary Tree - Leetcode
+
+# Given the root of a binary tree, return its maximum depth.
+
+class TreeNode:
+    def __init__(self, val=0, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
