25fd839160dbab73f20711a955ad6c964312d352
pairofAces <karanchauhan16@gmail.com>
2021-06-23 20:17:47 -0400

    recursive solution done

--- Problems/Easy/maxDepthOfBinaryTree.py
+++ Problems/Easy/maxDepthOfBinaryTree.py
@@ -16,4 +16,5 @@ class Solution:
             return 0
         
         # if the root is not empty
-        
+            # recursive call on both children nodes
+        return (1 + max(self.maxDepth(self.left), self.maxDepth(self.right)))
