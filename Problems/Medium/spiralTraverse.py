2be7ed79e384e711d280f9e4400135e28bbb3075
pairofAces <karanchauhan16@gmail.com>
2021-07-12 22:36:24 -0400

    working on spiral traverse class Solution

---
+++ Problems/Medium/spiralTraverse.py
@@ -0,0 +1,13 @@
+# SPIRAL TRAVERSE
+
+# Create a function that takes in an (n) * (m) two-dimensional array (that
+# can be square-shaped when n == m) and returns a one-dimensional array
+# of all the array's elements in spiral order.
+
+    # Note:
+    # Sprial order starts at the top left corner of the two-dimensional array,
+    # then goes to the right, and proceeds in a spiral pattern until all the
+    # elements have been visited.
+
+class Solution:
+    def spiralTraverse(array):
