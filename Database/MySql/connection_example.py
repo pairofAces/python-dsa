9931cbfdfc1d58bb1ca883e56d4250989f597fc4
pairofAces <karanchauhan16@gmail.com>
2022-01-18 22:37:44 -0500

    generated comments about databse connection

--- Database/MySql/connection_example.py
+++ Database/MySql/connection_example.py
@@ -1,11 +1,23 @@
 # hypothetical example of connecting to a MySql database
+# ---------------------------------------------------------------------
+    # Establish a connection to the database
 
 import mysql.connector
 con = mysql.connector.connect( host = "host", user = "username", 
     passwd = "password", database = "database")
+# ---------------------------------------------------------------------
+    # Execute a query
+
 cur = con.cursor()
 cur.execute("INSERT INTO Computer VALUES (1005, 'Deli', 'Vostro', 2013)");
+
+# ---------------------------------------------------------------------
+    # print the results of the query
+
 print(cur.rowcount)
+# ---------------------------------------------------------------------
+    # clean up and close the connection
+    
 cur.close()
 con.commit()
 con.close()
