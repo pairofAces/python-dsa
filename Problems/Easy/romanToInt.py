42d1bad864043ee35a53213c0bed6f74e4bb4162
pairofAces <karanchauhan16@gmail.com>
2022-03-02 20:04:39 -0500

    created solution for roman to int

--- Problems/Easy/romanToInt.py
+++ Problems/Easy/romanToInt.py
@@ -1,14 +1,25 @@
 # roman to int Leetcode
 
-def romanToInt(s : str):
-    # create a dictionary for the roman numerals and the values
-    roman_map = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    }
-    
+class Solution:
+
+    def romanToInt(self, s : str):
+        # create a dictionary for the roman numerals and the values
+        roman_map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        _res = 0
+
+        for i in range(len(s) - 1):
+            if roman_map[s[i]] < roman_map[s[i + 1]]:
+                _res = _res - roman_map[s[i]]
+            else:
+                _res = _res + roman_map[s[i]]
+        
+        return _res + roman_map[s[len(s) - 1]]
