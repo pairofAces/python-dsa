7a59d0b3251e23393c714fb4156db15af319d7e1
pairofAces <karanchauhan16@gmail.com>
2022-03-16 22:32:03 -0400

    created question template in medium folder

---
+++ Problems/Medium/reorderRoutes.py
@@ -0,0 +1,19 @@
+# Leetcode 1466
+
+# Reorder routes to make all paths lead to city zero
+
+# there are (n) cities numbered 0 to (n-1) and (n-1) roads such that
+# there is only one way to travel between cities. 
+# Last year, the ministry of transport decided to orient the roads in one direction
+# because they're too narrow. 
+
+# Roads are represented by (connections) where connections[i] = [a ,b]
+# represents a road from city (a) to (b).
+
+# There's going to be a big event in (city 0) 
+
+# your task consists of reorienting the roads so that each city can visit
+# (city 0). Return the minimum number of edges changed.
+
+# # it's guaranteed that each city can reach (city 0) after the reorder
+
