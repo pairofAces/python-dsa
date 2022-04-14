891947e86a84105a3e4a0556d784a94f502a4b09
pairofAces <karanchauhan16@gmail.com>
2022-04-13 22:21:18 -0400

    create folder and initial file for blind75 problems

---
+++ Blind75/containsDuplicate.py
@@ -0,0 +1,14 @@
+# Leetcode 217 - Contains Duplicate
+
+# Given an integer array (nums), return true if any value appears at least
+# twice in the array, and return false if every element is unique.
+
+class Solution:
+    def containsDuplicate(self, nums : List[int]) -> bool:
+        hashset = set()
+
+        for i in nums:
+            if i in hashset:
+                return True
+            hashset.add(i)
+        return False
