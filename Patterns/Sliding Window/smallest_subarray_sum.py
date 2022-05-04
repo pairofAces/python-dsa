4ce9886db2eb37142a05a37caa40084804506c33
pairofAces <karanchauhan16@gmail.com>
2022-05-03 22:54:22 -0400

    initial commit for smallest subarray

---
+++ Patterns/Sliding Window/smallest_subarray_sum.py
@@ -0,0 +1,7 @@
+# Given an array of integers and a positive number (s), find the length
+# of the smallest contiguous subarray greater than or equal to
+# (s). 
+# return 0 is there isn't any
+
+def smallest_subarray_sum(s, array):
+    
