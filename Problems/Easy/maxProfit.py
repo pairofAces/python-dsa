54998901a6692b5a109fbe24339909922aa0dbe3
pairofAces <karanchauhan16@gmail.com>
2022-05-23 22:24:29 -0400

    created file to find max profit

---
+++ Problems/Easy/maxProfit.py
@@ -0,0 +1,12 @@
+# Leetcode 121 - Best Time to Buy and Sell Stock
+
+# say you have an array for which the (i)th element is the 
+# price of a given stock on day (i)
+
+# if you're only permitted to complete at most one transaction (
+# i.e. buy one and sell one share of the stock), design an
+# algorithm to find the max profit. 
+
+# NOTE - cannot sell a stock before buying one
+
+def maxProfit(array):
